1fcb8bf89c3b4844f22c6d3f4b526da0ab2478e7
Marcelo Ferranti <mferranti@Mac-de-Marcelo.local>
2018-06-05 23:50:20 -0300

    Adding methods to send private and public messages

--- index.js
+++ index.js
@@ -4,18 +4,62 @@ const superagent = require('superagent')
 
 const app = express()
 
+function checkError(routeHandler) {
+  return async (req, res, next) => {
+    try {
+      await routeHandler(req, res, next)
+    } catch (err) {
+      res.send({
+        status: err.status,
+        msg: err.message,
+      })
+      next(err)
+    }
+  }
+}
+
+const sendMessage = async (message, endpoint) => superagent.post(endpoint)
+  .send(message)
+  .set('accept', 'json')
+
 let chatApps = new Map;
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json())
 
 app.get('/', (req, res) => res.sendFile(`${__dirname}/README.md`))
-app.post('/integrate', (req, res) => {
-  const { id, endpoint, port } = req.body;
-  chatApps.set(id, { endpoint, port })
-  res.send({status: 200, msg: `App ${req.body.id} integrated successfully with endpoint: ${req.body.endpoint}`})
-})
-app.post('/send', async (req, res) => {
+app.get('/apps', (req, res) =>
+  res.send({
+    status: 200,
+    msg: Array.from(chatApps)
+  })
+)
+app.post('/integrate', checkError((req, res) => {
+  const { id, endpointPublic, endpointPrivate, endpointContacts } = req.body;
+  chatApps.set(id, { endpointPublic, endpointPrivate, endpointContacts });
+  res.send({
+    status: 200,
+    msg: `App ${req.body.id} integrated successfully integrated`,
+  })
+}))
+app.post('/public/send', checkError(async (req, res) => {
+  const { from, msg, to, attachment, sourceApp } = req.body;
+  // some validator for message?
+  const message = {
+    from,
+    msg,
+    to,
+    attachment,
+    sourceApp,
+  };
+  for ([key, chatApp] of chatApps) {
+    if (key !== sourceApp) {
+      await sendMessage(message, chatApp.endpointPublic)
+    }
+  }
+  res.send({status: 200, msg: 'Broadcast message sent succesfully'});
+}))
+app.post('/private/send', checkError(async (req, res) => {
   const { from, msg, to, attachment, sourceApp, targetApp } = req.body;
   // some validator for message?
   const message = {
@@ -24,25 +68,15 @@ app.post('/send', async (req, res) => {
     to,
     attachment,
     sourceApp,
-    targetApp,
   };
-  chatApps.forEach((chatApp, key) => {
-    if (key !== sourceApp) {
-      const { endpoint, port } = chatApp;
-      superagent.post(chatApp.endpoint)
-        .send(message)
-        .set('accept', 'json')
-        .end();
-    }
-  })
-
+  await sendMessage(message, chatApps.get(targetApp).endpointPrivate)
   res.send({status: 200, msg: 'Broadcast message sent succesfully'});
-})
+}))
 
-app.post('/test', (req, res) => {
+app.post('/test', checkError((req, res) => {
   console.log('message arrived from: ', req.body )
   res.send({...req.body})
-})
+}))
 
 const port = process.env.PORT || 5000;
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
